d4a5e99f8c51a68f81f764f3bdf83c51dac7ef54
junichi <sakai.jun01@gmail.com>
2021-06-02 17:29:28 +0200

    ver 0.2

--- worker/pascalWorker.js
+++ worker/pascalWorker.js
@@ -58,36 +58,6 @@ function autoSpace(max, div = 1) {
 }*/
 
 //Binomial Cofficient
-
-function pascal(input, callback) {
-  let maxVal=0;  
-  let arr2d =[];
-  for (let i =0; i < input.value; i++){    
-    arr2d[i] = [];
-    for(let j =0; j < i+1; j++){
-      arr2d[i][j] = parseInt(n_over_k(i,j));
-      if(i == input.value -1){
-        maxVal = n_over_k(i,parseInt(j/2));
-      }
-    }   
-  }   
-  (!input.mode)
-    ? callback(sierpinskiOutput(arr2d, input.value))
-    : callback(pascalOutput(arr2d, input.value, maxVal));
-}
-
-function n_over_k(n, k){
-  return fact(n)/(fact(k)*fact(n-k));
-}
-
-function fact(n){
-  let res = 1;
-  for(let i =1;i<=n; i++){
-    res *=i;
-  }
-  return res;
-}
-
 function pascal(input, callback){
     let maxVal = 0;
     arr2d = []
