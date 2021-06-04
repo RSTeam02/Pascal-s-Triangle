a0386586ef2c1c4e3af60c714ab298a8a9e9ac96
junichi <sakai.jun01@gmail.com>
2021-06-04 16:00:09 +0200

    binom coeff ver0.3

--- mvc/result.js
+++ mvc/result.js
@@ -4,21 +4,20 @@ export class Result{
         this.pascal = pascal;
     }
 
-    autoSpace(max, div = 1) {
-        let subSpace = "";       
-        for (let i = 0; i <= Math.floor(max / div); i++) {
-            subSpace += "\u0020";
-        }
-        return subSpace;
+    autoSpace(max, div = 1) {       
+        let subSpace = new Array(Math.floor(max / div)+1);
+        subSpace=subSpace.fill("\u0020",0);        
+        return subSpace.join("");
     }
 
     sierpinskiOutput() {
         let allRowStr = "";
         let maxValLen = this.pascal.maxVal.toString().length;
+        let subsubSpace = []
         for (let i = 0; i < this.pascal.arr2d.length; i++) {         
-            for (let k = this.pascal.input.value - i - 1; k > 0; k--) {
-                allRowStr += "\u0020";
-            }
+            subsubSpace = new Array(this.pascal.input.value - i - 1);
+            subsubSpace = subsubSpace.fill("\u0020",0);
+            allRowStr += subsubSpace.join("");
             for (let j = 0; j < this.pascal.arr2d[i].length; j++) {
                 (j&(i-j))
                     ? allRowStr += "\u0020\u0020"
@@ -30,12 +29,13 @@ export class Result{
     }
 
     pascalOutput() {
-        let allRowStr = "";
+        let allRowStr = "";      
+        let subsubSpace = []  
         let maxValLen = this.pascal.maxVal.toString().length;      
-        for (let i = 0; i < this.pascal.arr2d.length; i++) {         
-            for (let k = this.pascal.input.value - i - 1; k > 0; k--) {                
-                allRowStr += `${this.autoSpace(maxValLen, 2)}`;
-            }
+        for (let i = 0; i < this.pascal.arr2d.length; i++) {       
+            subsubSpace = new Array(this.pascal.input.value - i - 1);
+            subsubSpace = subsubSpace.fill(`${this.autoSpace(maxValLen, 2)}`,0);
+            allRowStr += subsubSpace.join("");
             for (let j = 0; j < this.pascal.arr2d[i].length; j++) {                
                 (j === 0)
                     ? allRowStr += this.pascal.arr2d[i][j]
